9ed1105a7fa17f3f14709216a33aa90d5117d86b
Yohei Yamamoto <yohei_yamamoto@dwango.co.jp>
2022-12-12 15:09:22 +0900

    タイマーカウント中に入力欄の値が変わらないように変数を分けた

--- src/App.tsx
+++ src/App.tsx
@@ -12,30 +12,34 @@ const Timer:React.FC<Props> = (props) => {
 }
 
 function App() {
-  const [time, setTime] = useState(100);
+  const [inputTime, setInputTime] = useState(100);
+  const [counter, setCounter] = useState(100);
   const [isTimerCounting, setIsTimerCounting] = useState(false);
   const [timerId, setTimerId] = useState(0);
   var interval: NodeJS.Timer
   return (
     <div className="App">
       <input type="number"
-      value={time}
-      onChange={(event) => setTime(Number(event.target.value))}/>
+      value={inputTime}
+      onChange={(event) => {
+        setInputTime(Number(event.target.value))
+        setCounter(Number(event.target.value))
+      }}/>
       <button onClick={
         () => {
           if(isTimerCounting === false) {
-            interval = setInterval(() => setTime(t => t-1), 1000)
-              setTimerId(Number(interval))
-              setIsTimerCounting(true)
-            }
+            interval = setInterval(() => setCounter(t => t-1), 1000)
+            setTimerId(Number(interval))
+            setIsTimerCounting(true)
           }
-        }>start</button>
+        }
+      }>start</button>
       <button onClick={() => {
         if(isTimerCounting === true) {
           clearInterval(timerId)
           setIsTimerCounting(false)
         }}}>stop</button>
-      <Timer time={time} />
+      <Timer time={counter} />
     </div>
   );
 }
